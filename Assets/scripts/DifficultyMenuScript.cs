7bb2b0b74efce26c3517784008599ba67ddadded
BRIANGL <brian.gl@eduge.ch>
2020-11-18 16:30:50 +0100

    ajout menu

---
+++ Assets/scripts/DifficultyMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyMenuScript : MonoBehaviour
+{
+    public string loadLevel;
+    public void startGame()
+    {
+        Application.LoadLevel(loadLevel);
+    }
+    public void BigTarget (){
+        loadLevel = "BigTarget";
+        startGame();
+    }
+    public void MediumTarget()
+    {
+        loadLevel = "MediumTarget";
+        startGame();
+    }
+    public void SmallTarget()
+    {
+        loadLevel = "SmallTarget";
+        startGame();
+    }
+
+    public void quitGame()
+    {
+
+        Application.Quit();
+
+
+    }
+}
